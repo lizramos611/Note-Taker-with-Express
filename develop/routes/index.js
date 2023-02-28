e51110bd1545e3862830c70a120d6adf4a1e5f8e
lizramos611 <111591265+lizramos611@users.noreply.github.com>
2023-02-28 15:19:59 -0500

    added notes

--- develop/routes/index.js
+++ develop/routes/index.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
-
+//routes to home page
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
-
+//routes to notes page
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
